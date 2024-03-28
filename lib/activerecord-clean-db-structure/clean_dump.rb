3f50fc0d081d1544b4b4faecdc8f8d7979131d01
Bob van Oorschot <10108934+bobvanoorschot@users.noreply.github.com>
2024-03-28 16:08:09 +0100

    Fix column sorting issue in clean_dump.rb

--- lib/activerecord-clean-db-structure/clean_dump.rb
+++ lib/activerecord-clean-db-structure/clean_dump.rb
@@ -146,7 +146,7 @@ module ActiveRecordCleanDbStructure
           $LAST_MATCH_INFO[:columns]
           .split(",\n")
           .sort_by { |column| column.delete('"') }
-          .partition { |column| !column.match?(/\A *CONSTRAINT|PRIMARY KEY/) }
+          .partition { |column| !column.match?(/\ACONSTRAINT|PRIMARY KEY/) }
           .flatten
           .join(",\n")
 
